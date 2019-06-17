a778341a5a571eb7801309b669fa04e91d8d49c1
datalogics-robl <robl@datalogics.com>
2019-06-17 16:07:40 -0500

    Commit initial version of the hot folder watcher script
    This version converts supported file types (excluding image types)
    from a fixed path and copies them to a fixed name. No logging.

---
+++ ConvertFile.ps1
@@ -0,0 +1,22 @@
+﻿$folder = 'C:\Users\robl\src\BlogArticle\watched_dir'
+$destinationFolder = 'C:\Users\robl\src\BlogArticle\destination_dir\'
+$outputName = $destinationFolder + 'output.pdf'
+
+$supportedTypes = ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".ps", ".eps" # TODO: add image types
+
+$fsw = New-Object IO.FileSystemWatcher $folder, "*.*" -Property @{
+  IncludeSubdirectories = $true
+  NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'
+}
+$onCreated = Register-ObjectEvent $fsw Created -SourceIdentifier FileConverted -Action {
+  $path = $Event.SourceEventArgs.FullPath
+  $name = $Event.SourceEventArgs.Name
+  $timeStamp = $Event.TimeGenerated
+  $extension = [System.IO.Path]::GetExtension($name).ToLower()
+ 
+  if ($supportedTypes.Contains($extension)) {
+    Write-Host "The file '$name' was converted at $timeStamp"
+    Invoke-Command -ScriptBlock { flip2pdf --input $path --output $outputName }
+    Move-Item $path -Destination $destinationFolder -Force -Verbose # Force will overwrite files with the same name
+  }
+}
