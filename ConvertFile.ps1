460e40336d7ae2eef2e0d8b5dea1c6d6717de1d3
datalogics-robl <robl@datalogics.com>
2019-06-18 12:17:46 -0500

    Log the result of each conversion
    We log the result of each conversion to a logfile in the destination
    folder, with the error code from flip2pdf for failures.
    
    Since we now log results, this also eliminates all messages to the
    console.

--- ConvertFile.ps1
+++ ConvertFile.ps1
@@ -1,5 +1,12 @@
 ﻿$folder = 'C:\Users\robl\src\BlogArticle\watched_dir'
 $destinationFolder = 'C:\Users\robl\src\BlogArticle\destination_dir\'
+$logpath = $destinationFolder + "convertfile.log"
+
+# Function to write to our log file
+function Write-Log {
+  param($message)
+  "$(Get-Date -Format G) : $message" | Out-File -FilePath $logpath -Append -Force
+}
 
 $officeTypes = ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx"
 $postscriptTypes = ".ps", ".eps"
@@ -10,16 +17,21 @@ $fsw = New-Object IO.FileSystemWatcher $folder, "*.*" -Property @{
   IncludeSubdirectories = $true
   NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'
 }
+
 $onCreated = Register-ObjectEvent $fsw Created -SourceIdentifier FileConverted -Action {
   $path = $Event.SourceEventArgs.FullPath
   $name = $Event.SourceEventArgs.Name
-  $timeStamp = $Event.TimeGenerated
   $extension = [System.IO.Path]::GetExtension($name).ToLower()
   $outputName = $destinationFolder + [System.IO.Path]::GetFileNameWithoutExtension($name) + ".pdf"
  
   if ($supportedTypes.Contains($extension)) {
-    Write-Host "The file '$name' was converted at $timeStamp"
     Invoke-Command -ScriptBlock { flip2pdf --input $path --output $outputName }
-    Move-Item $path -Destination $destinationFolder -Force -Verbose # Force will overwrite files with the same name
+    Move-Item $path -Destination $destinationFolder -Force # Force will overwrite files with the same name
+
+    if ($LASTEXITCODE -eq 0) {
+      Write-Log "$name successfully converted to $outputName"
+    } else {
+      Write-Log "Conversion of $name failed with error code: $LASTEXITCODE"
+    }
   }
 }
