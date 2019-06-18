1bf7445c33b471d33045d92163a860d6bfea1986
datalogics-robl <robl@datalogics.com>
2019-06-18 15:10:48 -0500

    Eliminate file filtering
    Since flip2pdf introspects files to determine their type, we don't
    need to filter files based on their extension: just feed all files
    to flip2pdf and handle types it can't convert as errors.
    
    Note that we do need to filter out files starting with '.~lock.':
    flip2pdf places these in the source folder during conversion (and
    removes them when conversion is complete), so we exit early to
    avoid trying to convert them as well.

--- ConvertFile.ps1
+++ ConvertFile.ps1
@@ -8,12 +8,7 @@ function Write-Log {
   "$(Get-Date -Format G) : $message" | Out-File -FilePath $logpath -Append -Force
 }
 
-$officeTypes = ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx"
-$postscriptTypes = ".ps", ".eps"
-$imageTypes = ".bmp", ".jpg", ".jpeg", ".png", ".tif", ".tiff"
-$supportedTypes = $officeTypes + $postscriptTypes + $imageTypes
-
-$fsw = New-Object IO.FileSystemWatcher $folder, "*.*" -Property @{
+$fsw = New-Object IO.FileSystemWatcher $folder, "*" -Property @{
   IncludeSubdirectories = $true
   NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'
 }
@@ -24,14 +19,18 @@ $onCreated = Register-ObjectEvent $fsw Created -SourceIdentifier FileConverted -
   $extension = [System.IO.Path]::GetExtension($name).ToLower()
   $outputName = $destinationFolder + [System.IO.Path]::GetFileNameWithoutExtension($name) + ".pdf"
  
-  if ($supportedTypes.Contains($extension)) {
-    $result = Invoke-Command -ScriptBlock { flip2pdf --input $path --output $outputName }
-    Move-Item $path -Destination $destinationFolder -Force # Force will overwrite files with the same name
+  # Ignore lock files
+  if ($name.StartsWith(".~lock.")) {
+    return
+  }
 
-    if ($LASTEXITCODE -eq 0) {
+  $result = Invoke-Command -ScriptBlock { flip2pdf --input $path --output $outputName }
+  Move-Item $path -Destination $destinationFolder -Force # Force will overwrite files with the same name
+
+  # Log results
+  if ($LASTEXITCODE -eq 0) {
       Write-Log "$name successfully converted to $outputName"
-    } else {
+  } else {
       Write-Log "Conversion of $name failed with error code: $LASTEXITCODE, message: $($result[5])"
-    }
   }
 }
